aecacd8ab55b386308d6272b37f0b09ecf349bcc
abhishekkashyap18 <117872546+abhishekkashyap18@users.noreply.github.com>
2024-06-03 18:35:11 +0530

    bytequest assignment

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,138 @@
-import Image from "next/image";
+"use client";
+import SearchOutlinedIcon from "@mui/icons-material/SearchOutlined";
+import PersonOutlineOutlinedIcon from "@mui/icons-material/PersonOutlineOutlined";
+import BookmarkBorderOutlinedIcon from "@mui/icons-material/BookmarkBorderOutlined";
+import ShoppingBagOutlinedIcon from "@mui/icons-material/ShoppingBagOutlined";
+import CasesOutlinedIcon from "@mui/icons-material/CasesOutlined";
+import BackpackOutlinedIcon from "@mui/icons-material/BackpackOutlined";
+import ShoppingBagIcon from "@mui/icons-material/ShoppingBag";
+import BusinessCenterOutlinedIcon from "@mui/icons-material/BusinessCenterOutlined";
+import LocalMallOutlinedIcon from "@mui/icons-material/LocalMallOutlined";
+import LuggageOutlinedIcon from "@mui/icons-material/LuggageOutlined";
+import MedicalServicesOutlinedIcon from "@mui/icons-material/MedicalServicesOutlined";
+import IosShareOutlinedIcon from "@mui/icons-material/IosShareOutlined";
+
+import { useEffect, useState } from "react";
+import axios, { AxiosResponse } from "axios";
+
+interface Product {
+  id: number;
+  title: string;
+  image: string;
+  price: number;
+}
 
 export default function Home() {
+  const [products, setProducts] = useState<Product[]>([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState<Error | null>(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      setIsLoading(true);
+      setError(null);
+
+      try {
+        const response = await axios.get("https://fakestoreapi.com/products");
+        setProducts(response.data);
+        console.log(products);
+      } catch (err: any) {
+        setError(err);
+      } finally {
+        setIsLoading(false);
+      }
+    };
+
+    fetchData();
+  }, []);
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <div>
+      <div className=" flex flex-row text-white w-full justify-between">
+        <h1 className=" text-2xl pl-8 pt-3  font-extralight">TAN TRIM</h1>
+
+        <ul className="flex flex-row gap-14 py-14">
+          <li>Bags</li>
+          <li>Travel</li>
+          <li>Accesories</li>
+          <li>Gifting</li>
+          <li>Jewelery</li>
+        </ul>
+
+        <ul className="flex flex-row gap-5 pr-8 pt-3">
+          <li>
+            <SearchOutlinedIcon />
+          </li>
+          <li>
+            <PersonOutlineOutlinedIcon />
+          </li>
+          <li>
+            <BookmarkBorderOutlinedIcon />
+          </li>
+          <li>
+            <ShoppingBagOutlinedIcon />
+          </li>
+        </ul>
+      </div>
+
+      <div className=" py-5">
+        <ul className="flex flex-row justify-between w-full px-8">
+          <li className="flex flex-col items-center">
+            <BackpackOutlinedIcon className=" size-24" /> <label>Bag</label>
+          </li>
+          <li className="flex flex-col items-center">
+            <CasesOutlinedIcon className=" size-24" />
+            <label>Bag</label>
+          </li>
+          <li className="flex flex-col items-center">
+            <ShoppingBagIcon className=" size-24" />
+            <label>Bag</label>
+          </li>
+          <li className="flex flex-col items-center">
+            <BusinessCenterOutlinedIcon className=" size-24" />
+            <label>Bag</label>
+          </li>
+          <li className="flex flex-col items-center">
+            <LocalMallOutlinedIcon className=" size-24" />
+            <label>Bag</label>
+          </li>
+          <li className="flex flex-col items-center">
+            <LuggageOutlinedIcon className=" size-24" />
+            <label>Bag</label>
+          </li>
+          <li className="flex flex-col items-center">
+            <MedicalServicesOutlinedIcon className=" size-24" />
+            <label>Bag</label>
+          </li>
+        </ul>
+        <div className="flex flex-row justify-between py-8 px-5">
+          <p>Bags . BagPacks</p>
+          <p>
+            13 Products{" "}
+            <span className="px-1">
+              <IosShareOutlinedIcon className="pb-1" />
+            </span>
+          </p>
         </div>
       </div>
 
-      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
+      <div className="product-list flex flex-row flex-wrap gap-10 px-8">
+        {isLoading && <p className="">Hold on tight! Loading products...</p>}
+        {error && <p>Error fetching products: {error.message}</p>}
+        {products.map((product) => (
+          <div className="card bg-gray-950 rounded-lg shadow-md overflow-hidden w-52 h-96" key={product.id}>
+            <img 
+            src={product.image} 
+            alt={product.title} 
+            className="card-image bg-white w-52 h-60 overflow-hidden" />
+            <div className="card-content text-white pt-6">
+              <h3 className="card-title text-sm">{product.title}</h3>
+              <p className="card-price">₹{product.price.toFixed(2)}<span className=" stroke-slate-800 pl-2 text-gray-600 line-through">899<span className=" text-green-500">(50% off)</span></span> <span className=" pl-2"><LocalMallOutlinedIcon/></span></p>{" "}
+              {/* Format price to two decimals */}
+            </div>
+          </div>
+        ))}
       </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+    </div>
   );
 }
